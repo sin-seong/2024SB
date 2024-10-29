003d317501ee12d285d23e3574e003039a9399e9
sin-seong <leeshoma@gmail.com>
2024-10-29 17:29:07 +0900

    1213

---
+++ sb1029/src/main/java/com/sin/sb1029/Sb1029Application.java
@@ -0,0 +1,13 @@
+package com.sin.sb1029;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1029Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb1029Application.class, args);
+    }
+
+}
---
+++ sb1029/src/test/java/com/sin/sb1029/Sb1029ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sin.sb1029;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb1029ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
